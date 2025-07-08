fd753562007ae27cae2f1cab80a793dce3b009af
ShawnSomething <khooweishawn@gmail.com>
2025-07-09 07:06:21 +1000

    groups

--- asteriod-bootdotdev/main.py
+++ asteriod-bootdotdev/main.py
@@ -6,6 +6,10 @@ def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
+
+    updateable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updateable, drawable)
     dt = 0
     x = SCREEN_WIDTH / 2
     y = SCREEN_HEIGHT / 2   
@@ -15,9 +19,10 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        player.update(dt)
+        updateable.update(dt)
         screen.fill("black")
-        player.draw(screen)
+        for obj in drawable:
+            obj.draw(screen)
         pygame.display.flip()
         dt = clock.tick(60) / 1000
 
