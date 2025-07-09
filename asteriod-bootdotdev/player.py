9f9e5a93832aa211a0187f9b176cb80fab5adfb9
ShawnSomething <khooweishawn@gmail.com>
2025-07-10 09:12:36 +1000

    rate limit

--- asteriod-bootdotdev/player.py
+++ asteriod-bootdotdev/player.py
@@ -7,6 +7,7 @@ class Player(CircleShape):
     def __init__(self, x, y):
         super().__init__(x, y, PLAYER_RADIUS)
         self.rotation = 0
+        self.timer = 0
 
     def triangle(self):
         forward = pygame.Vector2(0, 1).rotate(self.rotation)
@@ -23,24 +24,28 @@ class Player(CircleShape):
         self.rotation += PLAYER_TURN_SPEED * dt
     
     def update(self, dt, screen):
+        self.timer -= dt
         keys = pygame.key.get_pressed()
 
         if keys[pygame.K_a]:
             self.rotate(-dt)
-        elif keys[pygame.K_d]:
+        if keys[pygame.K_d]:
             self.rotate(dt)
-        elif keys[pygame.K_w]:
+        if keys[pygame.K_w]:
             self.move(dt)
-        elif keys[pygame.K_s]:
+        if keys[pygame.K_s]:
             self.move(-dt)
         
         if keys[pygame.K_SPACE]:
             self.shoot(screen)
+            self.timer = PLAYER_SHOOT_COOLDOWN
     
     def move(self, dt):
         forward = pygame.Vector2(0, 1).rotate(self.rotation)
         self.position += forward * PLAYER_SPEED * dt
     
     def shoot(self, screen):
+        if self.timer > 0:
+            return
         shot = Shot(self.position.x, self.position.y, SHOT_RADIUS)
         shot.velocity = pygame.Vector2(0, 1).rotate(self.rotation) * PLAYER_SHOOT_SPEED
